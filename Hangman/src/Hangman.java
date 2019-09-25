8998c648b2b01182d44fecc36336a6bbf3197f9d
OzzieM <54642856+OzzieM@users.noreply.github.com>
2019-09-25 12:24:47 -0700

    Add files via upload

--- Hangman/src/Hangman.java
+++ Hangman/src/Hangman.java
@@ -19,27 +19,41 @@ public class Hangman {
     }
 
     public String Guessing(){
+
         Scanner guess = new Scanner(System.in);
-        System.out.print("Enter your guess: ");
-        return guess.nextLine();
-        //return letterGuess;
+        String a ="";
+        while(!a.toUpperCase().matches("[A-Z]")){
+            System.out.print("Enter your guess: ");
+            a = guess.nextLine();
+            if(a.toUpperCase().matches("[A-Z]")){
+                return a;
+            }
+            else{
+                System.out.println("Incorrect Input");
+            }
+        }
+        return  a;
     }
 
-    public int Hint(int hint){
-        return hint;
+    public String Hint(){
+        for(int i=0; i<lul.size();i++){
+            if(!track.contains(lul.get(i)) && !lul.get(i).equals("#")){
+                return lul.get(i);
+            }
+        }
+        return null;
     }
 
     public void Word(String[] word){
-
-
-
-
-
-        for(String i : lul){
-            System.out.print(" "+i);
+        for(int i = 0; i<word.length;i++){
+            if(word[i].equals(" ")){
+                lul.add("#");
+            }
+            else{
+                lul.add(word[i]);
+            }
         }
-        //return null;
-        }
+    }
 
 
     public static void main(String[] args) {
@@ -50,66 +64,67 @@ public class Hangman {
         int guesses = 4;
 
         Hangman hang = new Hangman();
-            String a = hang.Scanner();
-            //a = a.replace(" ","#");
-            String[] b = a.split("");
+            String a = hang.Scanner().toUpperCase(); //scan input and change to UpperCase
+            hang.Word(a.split("")); // split string and add each letter to arraylist
 
-        for(int i = 0; i<b.length;i++){
-            if(b[i].equals(" ")){
-                lul.add(i,"#");
-            }
-            else {
-                lul.add(i,"_");
-            }
-        }
+        //System.out.println(lul);
 
         while(guesses != 0){
+            //winning condition
+            if(track.containsAll(lul)){
+                break;
+            }
+            System.out.println();
             System.out.print("So far, the word is: ");
-
-            for (int i = 0; i<track.size(); i++){
-                if(b[i].contains(track.get(i))){
-                    lul.set(i,b[i]);
+                // prints blanks/letters
+            for(int i =0;i<lul.size();i++){
+                if(lul.get(i).equals("#")){
+                    System.out.print(" #");
+                }
+                else if(!track.contains(lul.get(i))){
+                    System.out.print(" _");
+                }
+                else if(track.contains(lul.get(i))){
+                    System.out.print(" "+lul.get(i));
                 }
             }
 
-            for(String i : lul){
-                System.out.print(" "+i);
-            }
 
-            //hang.Word(b);
-
-//            for(int i = 0;i<b.length;i++){
-//                    if(!b[i].equals("#")){
-//                        System.out.print(" _");
-//                    }
-//                    else{
-//                        System.out.print(" #");
-//                    }
-//
-//
-//
-//            }
             System.out.println();
 
             Scanner oneORtwo = new Scanner(System.in);
-            System.out.println("You have " + guesses +" left.");
+            System.out.println("You have " + guesses +" incorrect guesses left.");
             boolean tf=true;
             while (tf){
                 System.out.print("Enter either 1 for guessing or 2 for hint: ");
-                int ot = oneORtwo.nextInt();
-                if(ot == 1){
-                    String letter = hang.Guessing();
-                    for(int i = 0; i<b.length; i++){
-                        if(b[i].equals(letter) ){
+                String ot = oneORtwo.nextLine();
+                if(ot.equals("1")){
+                    String letter = hang.Guessing().toUpperCase();
+                    for(int i = 0; i<lul.size(); i++){
+                        if(track.contains(letter)){
+                            System.out.println("Not Valid Input. You already guessed " + letter);
+                            tf = false;
+                            break;
+                        }
+                        else if(lul.contains(letter)){
                             System.out.println("That's right! "+ letter +" is in the word.");
                             tf = false;
+                            break; //was printing above line multiple times so broke to show only one occurrence
+                        }
+                        else if(!lul.contains(letter)){
+                            System.out.println("Sorry, "+ letter +" isn't in the word");
+                            guesses--;
+                            tf = false;
+                            break;
                         }
                     }//add guesses into an ArrayList to track
                     hang.Guesses(letter);
 
                 }//hint if int == 2
-                else if(ot == 2){
-
+                else if(ot.equals("2")){
+                    hang.Guesses(hang.Hint());
+                    guesses--;
+                    break;
                 }
                 else{
                     System.out.println("Incorrect Input.");
@@ -117,9 +132,20 @@ public class Hangman {
             }
 
         }
+        System.out.println();
 
-
-
+        if(track.containsAll(lul)){ //kind of redundant but double checks before outputting game winning condition
+            System.out.print("CONGRATULATIONS THE WORD WAS: ");
+            for(String i :lul){
+                System.out.print(i);
+            }
+        }
+        if(guesses == 0){
+            System.out.print("You Failed. The word was: ");
+            for(String i :lul){
+                System.out.print(i);
+            }
+        }
 
 
     }
